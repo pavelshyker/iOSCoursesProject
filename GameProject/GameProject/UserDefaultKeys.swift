28098449177a5d50d7bd272d271a5ecac8c04cac
Pavel Shyker <shikerpavel@gmail.com>
2021-02-20 23:10:25 +0300

    initial commit

---
+++ GameProject/GameProject/UserDefaultKeys.swift
@@ -0,0 +1,20 @@
+//
+//  UserDefaultKeys.swift
+//  GameProject
+//
+//  Created by Pavel Shyker on 10/26/20.
+//  Copyright © 2020 Pavel Shyker. All rights reserved.
+//
+
+import Foundation
+
+enum UserDefaultKeys: String {
+    case carColor = "selectedCarColor"
+    case userName = "userName"
+    case score = "userScore"
+    case record = "recordResult"
+    case date = "recordDate"
+    case sound = "soundOption"
+    case soundRestart = "isSoundShouldBeRestarted"
+    case accelerometer = "isAccelerometerNeeded"
+}
